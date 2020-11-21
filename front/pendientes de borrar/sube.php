f18d618d2e50ae2f727662ee2a9eeb27915da6da
Alex Oficina <github@mycodes4you.com>
2020-11-20 18:01:44 -0600

    Restaurando

--- front/pendientes de borrar/sube.php
+++
@@ -1,5 +0,0 @@
-<form action="../alumno_foto.php" enctype="multipart/form-data" method="post" id="form_cambia_foto">
-<input type="file" id="alumno_foto" class="form-control" />
-<input type="text" name="alumno_id" id="alumno_id" value="10">
-<button type="submit" class="btn btn-success" >Subir Foto</button>
-</form>
--- lista_profesores.php
+++
@@ -1,4 +0,0 @@
-<?php
-foreach($_POST as $k => $v){$$k=$v;}  echo $k.' -> '.$v.' | ';
-foreach($_GET as $k => $v){$$k=$v;}  echo $k.' -> '.$v.' | ';
-?>
